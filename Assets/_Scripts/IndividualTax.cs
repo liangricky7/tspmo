0755f5f08e04ce946e1b65b1598f0de63b160e1e
Ricky Liang <liangricky7@gmail.com>
2025-03-22 10:42:13 -0500

    1040 and W2 Base Data

---
+++ Assets/_Scripts/IndividualTax.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndividualTax : MonoBehaviour
+{
+    public string firstName;
+    public string lastName;
+    public string address;
+    public string city;
+    public string state;
+    public string filingStatus;
+    public string[] dependents;
+}
---
+++ Assets/_Scripts/W2.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class W2 : MonoBehaviour
+{
+    public string employerID;
+    public string employerName;
+    public string employerAddress;
+    public string employeeName;
+    public string employeeAddress;
+    public int wages;
+    
+}
