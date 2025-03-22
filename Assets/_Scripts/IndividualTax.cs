5a099e389729bf7e8d5a4dbbc9126a559f0f0755
Ricky Liang <liangricky7@gmail.com>
2025-03-22 12:24:12 -0500

    Added W2 and 1040 Prefabs

--- Assets/_Scripts/IndividualTax.cs
+++ Assets/_Scripts/IndividualTax.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IndividualTax : MonoBehaviour
 {
+    [Header("Data")]
     public string firstName;
     public string lastName;
     public string address;
@@ -11,4 +14,24 @@ public class IndividualTax : MonoBehaviour
     public string state;
     public string filingStatus;
     public string[] dependents;
+    [Space(20)]
+    [Header("Text Connections")]
+
+    public TextMeshProUGUI firstNameHolder;
+    public TextMeshProUGUI lastNameHolder;
+    public TextMeshProUGUI addressHolder;
+    public TextMeshProUGUI cityHolder;
+    public TextMeshProUGUI stateHolder;
+    public TextMeshProUGUI filingStatusHolder;
+    public TextMeshProUGUI[] dependentsHolder;
+
+    void Start()
+    {
+        firstNameHolder.text = firstName;
+        lastNameHolder.text = lastName;
+        addressHolder.text = address;
+        cityHolder.text = city;
+        stateHolder.text = state;
+        filingStatusHolder.text = filingStatus;
+    }
 }
--- Assets/_Scripts/W2.cs
+++ Assets/_Scripts/W2.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class W2 : MonoBehaviour
 {
+    [Header("Data")]
     public string employerID;
     public string employerName;
     public string employerAddress;
     public string employeeName;
     public string employeeAddress;
     public int wages;
+
+    [Space(20)]
+    [Header("Text Connections")]
+
+    public TextMeshProUGUI firstNameHolder;
+    public TextMeshProUGUI lastNameHolder;
+    public TextMeshProUGUI addressHolder;
+    public TextMeshProUGUI cityHolder;
+    public TextMeshProUGUI stateHolder;
+    public TextMeshProUGUI filingStatusHolder;
+    public TextMeshProUGUI[] dependentsHolder;
+
     
 }
