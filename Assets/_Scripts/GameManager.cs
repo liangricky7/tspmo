033f95c2f7e30ca217d1e5f5b3abf62654515c45
Ricky Liang <liangricky7@gmail.com>
2025-03-22 14:46:02 -0500

    Added Singleton to GameManager

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+    void Awake()
+    {
+        if (instance != null && instance != this) {
+            Destroy(this);
+        } else {
+            instance = this;
+        }
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
