72e52414c254ee66bba89f1aab065cc5837d58d7
Ricky Liang <liangricky7@gmail.com>
2025-03-22 13:08:03 -0500

    Implemented W2 setup

--- Assets/_Scripts/W2.cs
+++ Assets/_Scripts/W2.cs
@@ -7,23 +7,46 @@ using UnityEngine.UI;
 public class W2 : MonoBehaviour
 {
     [Header("Data")]
-    public string employerID;
-    public string employerName;
-    public string employerAddress;
-    public string employeeName;
+    public string fullEmployeeName;
+    public string employeeSSN;
     public string employeeAddress;
+    public string fullEmployerName;
+    public string employerSSN;
+    public string employerAddress;
     public int wages;
+    public int fedTax;
+    public int socialSecurity;
+    public int withheldMedicare;
+    public int netWages;
+    
 
-    [Space(20)]
+    [Space(10)]
     [Header("Text Connections")]
 
-    public TextMeshProUGUI firstNameHolder;
-    public TextMeshProUGUI lastNameHolder;
-    public TextMeshProUGUI addressHolder;
-    public TextMeshProUGUI cityHolder;
-    public TextMeshProUGUI stateHolder;
-    public TextMeshProUGUI filingStatusHolder;
-    public TextMeshProUGUI[] dependentsHolder;
+    public TextMeshProUGUI employeeNameHolder;
+    public TextMeshProUGUI employeeSSNHolder;
+    public TextMeshProUGUI employeeAddressHolder;
+    public TextMeshProUGUI employerNameHolder;
+    public TextMeshProUGUI employerSSNHolder;
+    public TextMeshProUGUI employerAddressHolder;
+    public TextMeshProUGUI wageHolder;
+    public TextMeshProUGUI fedTaxHolder;
+    public TextMeshProUGUI socSecHolder;
+    public TextMeshProUGUI withheldMedicareHolder;
+    public TextMeshProUGUI netWageHolder;
 
-    
+    void Start()
+    {
+        employeeNameHolder.text = fullEmployeeName;
+        employeeSSNHolder.text = employeeSSN;
+        employeeAddressHolder.text = employeeAddress;
+        employerNameHolder.text = fullEmployerName;
+        employerSSNHolder.text = employerSSN;
+        employerAddressHolder.text = employerAddress;
+        wageHolder.text = wages.ToString();
+        fedTaxHolder.text = fedTax.ToString();
+        socSecHolder.text = socialSecurity.ToString();
+        withheldMedicareHolder.text = withheldMedicare.ToString();
+        netWageHolder.text = netWages.ToString();
+    }
 }
