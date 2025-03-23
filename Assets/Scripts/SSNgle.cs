a5f9cadd167929e01742b156e99affbceba0ac70
Ricky Liang <liangricky7@gmail.com>
2025-03-23 12:28:30 -0500

    SSNGLE

--- Assets/Scripts/SSNgle.cs
+++ Assets/Scripts/SSNgle.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,24 +8,30 @@ public class SSNgle : MonoBehaviour
 {
     public GameObject searchBar;
     public Button searchToggle;
+    public TMP_InputField input;
     private bool toggle;
     public string search;
 
     void Start()
     {
-        toggle = false;
-        searchBar.SetActive(toggle);
+        input = searchBar.GetComponent<TMP_InputField>();
+        // toggle = false;
+        // searchBar.SetActive(toggle);
     }
 
-    public void DisplaySSNgle()
-    {
-        toggle = !toggle;
-        searchBar.SetActive(toggle);
-    }
+    // public void DisplaySSNgle()
+    // {
+    //     toggle = !toggle;
+    //     searchBar.SetActive(toggle);
+    // }
 
     public void getInput(string s) {
         search = s;
-        Debug.Log(search);
+        Debug.Log(input.text);
+        Identity identity = SSNDB.instance.Search(input.text);
+        Debug.Log(identity.name);
+        Debug.Log(identity.address);
+        Debug.Log(identity.age);
     }
 
 }
